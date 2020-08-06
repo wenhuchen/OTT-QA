e1d515ae18aa18e2fab2fe5348434ea4825f8122
wenhu chen <wenhuchen@cs.ucsb.edu>
2020-08-06 10:55:13 -0700

    Update preprocess_totto_tables.py

--- preprocess_totto_tables.py
+++ preprocess_totto_tables.py
@@ -1,17 +1,71 @@
+"""TOTTO table processsing.
+
+Processing the data format in TOTTO into the HybridQA one
+"""
+
 import copy
+import hashlib
 import json
+import multiprocessing
+from multiprocessing import Pool
+import re
+
+import nltk
+
+
+def clean_cell_text(string):
+  """Strip off the weird tokens."""
+  string = string.replace('"', '')
+  string = string.rstrip('^')
+  string = string.replace('–', '-')
+  string = string.replace('( ', '(')
+  string = string.replace(' )', ')')
+  string = string.replace('"', '')
+  string = string.replace(u'\u00a0', u' ')
+  string = string.replace('\n', ' ')
+  string = string.rstrip('^')
+  string = string.replace('\u200e', '')
+  string = string.replace('\ufeff', '')
+  string = string.replace(u'\u2009', u' ')
+  string = string.replace(u'\u2010', u' - ')
+  string = string.replace(u'\u2011', u' - ')
+  string = string.replace(u'\u2012', u' - ')
+  string = string.replace(u'\u2013', u' - ')
+  string = string.replace(u'\u2014', u' - ')
+  string = string.replace(u'\u2015', u' - ')
+  string = string.replace(u'\u2018', u'')
+  string = string.replace(u'\u2019', u'')
+  string = string.replace(u'\u201c', u'')
+  string = string.replace(u'\u201d', u'')
+  string = re.sub(r' +', ' ', string)
+  string = string.strip()
+  string = nltk_tokenize(string)
+  return string
+
+
+def hash_string(string):
+  sha = hashlib.sha256()
+  sha.update(string.encode())
+  return sha.hexdigest()[:16]
+
+
+def nltk_tokenize(string):
+  toks = nltk.word_tokenize(string)
+  return ' '.join(toks)
+
 
 def transform(original_table, debug=False):
   start_index = 0
   while start_index < len(original_table):
-    if not original_table[start_index][0]['is_header']:
+    if len(original_table[start_index]
+          ) <= 1 or not original_table[start_index][0]['is_header']:
       start_index += 1
     else:
       break
   if start_index >= len(original_table):
     raise NotImplementedError()
   if debug:
-    print("starting from {}th row".format(start_index))
+    print('starting from {}th row'.format(start_index))
 
   reserved = {}
   headers = []
@@ -19,96 +73,171 @@ def transform(original_table, debug=False):
   while start_index < len(original_table):
     row = copy.copy(original_table[start_index])
     tmp = []
-    
-    if len(headers) > 0 and len(reserved) == 0:
+
+    if headers and (not reserved):
       break
-    
+
     for j in range(0, total_columns):
       if j < len(tmp):
         continue
 
       if j in reserved:
-        current_idx = len(tmp)
-        tmp.append(reserved[current_idx][0])
-        reserved[current_idx][1] -= 1
-        if reserved[current_idx][1] == 0:
-          del reserved[current_idx]
+        tmp.append(reserved[j][0])
+        reserved[j][1] -= 1
+        if reserved[j][1] == 0:
+          del reserved[j]
       else:
-        if len(row) == 0 and len(headers) == 0:
+        if (not row) and (not headers):
           # The first row needs to determine the maximum column number
           total_columns = len(tmp)
           break
         else:
+          if not row:
+            raise NotImplementedError()
           cell = row.pop(0)
+          value = clean_cell_text(cell['value'])
           if cell['is_header']:
-            tmp.extend([cell['value']] * cell['column_span'])
+            tmp.extend([value] * cell['column_span'])
 
-            if cell['row_span'] > 1:
-              reserved[j] = [cell['value'], cell['row_span'] - 1]
-              if cell['column_span'] > 1:
-                for k in range(1, cell['column_span']):
-                  reserved[j + k] =  [cell['value'], cell['row_span'] - 1]
+          if cell['row_span'] > 1 and cell['row_span'] < 10:
+            reserved[j] = [value, cell['row_span'] - 1]
+            if cell['column_span'] > 1:
+              for k in range(1, cell['column_span']):
+                reserved[j + k] = [value, cell['row_span'] - 1]
 
-    if len(headers) == 0:  
+    if not headers:
       headers.extend(tmp)
     else:
-      for i in range(len(headers)):
-        headers[i] += ' - ' + tmp[i]
+      if len(headers) == len(tmp):
+        for i in range(len(headers)):
+          headers[i] += ' - ' + tmp[i]
 
     start_index += 1
   if debug:
-    print("Finished with headers: {}".format(headers))
+    print('Finished with headers: {}'.format(headers))
 
   if start_index >= len(original_table):
     raise NotImplementedError()
 
   total_columns = len(headers)
 
-  data = original_table[start_index:]
   rows = []
   reserved = {}
-  for i, row in enumerate(data):
-    row = copy.copy(row)
+  mapping = {}
+  for i in range(start_index, len(original_table)):
+    row = copy.copy(original_table[i])
     tmp = []
-    col_idx = 0
+    counter = 0
     for j in range(total_columns):
       if j < len(tmp):
         continue
 
       if j in reserved:
-        current_idx = len(tmp)
-        tmp.append(reserved[current_idx][0])
-        reserved[current_idx][1] -= 1
-        if reserved[current_idx][1] == 0:
-          del reserved[current_idx]
+        tmp.append(reserved[j][0])
+        reserved[j][1] -= 1
+        if reserved[j][1] == 0:
+          del reserved[j]
       else:
-        assert len(row) > 0
+        if not row:
+          raise NotImplementedError()
+        mapping[(i, counter)] = (len(rows), j)
         cell = row.pop(0)
-        tmp.extend([cell['value']] * cell['column_span'])
+        counter += 1
+        value = clean_cell_text(cell['value'])
+        tmp.extend([value] * cell['column_span'])
 
-        if cell['row_span'] > 1:
-          reserved[j] = [cell['value'], cell['row_span'] - 1]
+        if cell['row_span'] > 1 and cell['row_span'] < 10:
+          reserved[j] = [value, cell['row_span'] - 1]
           if cell['column_span'] > 1:
             for k in range(1, cell['column_span']):
-              reserved[j + k] =  [cell['value'], cell['row_span'] - 1]
-    
-    assert len(row) == 0
+              reserved[j + k] = [value, cell['row_span'] - 1]
+
+    if row:
+      raise NotImplementedError()
     rows.append(tmp)
 
-  return headers, rows
-  
-if __name__ == '__main__':
-  error = 0
-  output_dict = {}
-  for i in range(len(pair)):
-    try:
-      header, data = transform(pair[i]['table'])
-      table_idx = pair[i]['table_page_title'] + '_{}'.format(pair[i]['example_id'])
-      output_dict[table_idx] = {'header': header, 'data': data, 'url': pair[i]['table_webpage_url'], 'title': pair[i]['table_page_title'], 'section_title': pair[i]['table_section_title'], 'section_text': pair[i]['table_section_text'], 'intro': ''}
-    except Exception:
-      error += 1
-      #print("error with {}".format(i))
-  print("failing conversion rate = {}".format(error / len(pair)))
+  return headers, rows, mapping
 
-  with open('data/totto_tables.json', 'w') as f:
+
+def get_table_sent(entry):
+  try:
+    header, data, mapping = transform(entry['table'])
+    table_idx = entry['table_page_title'].replace(' ', '_') + '_{}'.format(
+        entry['example_id'])
+    table = (table_idx, {
+        'header': header,
+        'data': data,
+        'url': entry['table_webpage_url'],
+        'title': entry['table_page_title'],
+        'section_title': entry['table_section_title'],
+        'section_text': entry['table_section_text'],
+        'intro': ''
+    })
+
+    positive_cell = []
+    for cell in entry['highlighted_cells']:
+      if tuple(cell) in mapping:
+        index = mapping[tuple(cell)]
+        positive_cell.append([data[index[0]][index[1]], index, '', 'table'])
+
+    questions = []
+    if positive_cell:
+      for example in entry['sentence_annotations']:
+        sentence = clean_cell_text(example['final_sentence'])
+        hash_code = hash_string(sentence)
+        questions.append({
+            'table_id': table_idx,
+            'question': sentence,
+            'answer-text': 'none',
+            'answer-node': positive_cell,
+            'version': 'TOTTO',
+            'question_id': hash_code,
+            'where': 'table'
+        })
+    return table, questions
+
+  except NotImplementedError:
+    return None, None
+
+
+if __name__ == '__main__':
+  filepath = '/usr/local/google/home/wenhuchen/Documents/TOTTO/totto_data/totto_dev_data.jsonl'
+  pair = []
+  with open(filepath, 'r') as f:
+    for line in f:
+      pair.append(json.loads(line))
+  filepath = '/usr/local/google/home/wenhuchen/Documents/TOTTO/totto_data/totto_train_data.jsonl'
+  with open(filepath, 'r') as f:
+    for line in f:
+      pair.append(json.loads(line))
+  print('Finish loading local data')
+
+  output_dict = {}
+  sentences = []
+
+  cpu_cores = multiprocessing.cpu_count()
+  print('using {} cores'.format(cpu_cores))
+  pool = Pool(cpu_cores)
+
+  results = pool.map(get_table_sent, pair)
+  print('Finish the running')
+
+  error = 0
+  for r in results:
+    if r[0] and r[1]:
+      output_dict[r[0][0]] = r[0][1]
+      sentences.extend(r[1])
+    else:
+      error += 1
+
+  print('failing conversion rate = {}'.format(error / len(pair)))
+  print('successful tables = {}'.format(len(output_dict)))
+
+  with open(
+      '/usr/local/google/home/wenhuchen/Documents/TOTTO/totto_data//tables.json',
+      'w') as f:
     json.dump(output_dict, f, indent=2)
+  with open(
+      '/usr/local/google/home/wenhuchen/Documents/TOTTO/totto_data//train.json',
+      'w') as f:
+    json.dump(sentences, f, indent=2)
